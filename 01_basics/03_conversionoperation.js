c78ac0e2508987a9b4671482cbb3a612ef1f03c0
Harshitasharma05 <2024pietadharshita028@poornima.org>
2025-08-25 18:03:02 +0000

    operations conversion and comparison done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -26,3 +26,40 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//*****operations*****//
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hey"
+let str2 = "Nish"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
